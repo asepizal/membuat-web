e9c817cade6afcdc74f42115d46d615c86540853
asepizal <aseprizal2496@gmail.com>
2023-11-13 16:38:32 +0700

    trip plan

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navRow = document.querySelector('.nav__row')
   const navToggle = document.querySelector('.nav__toggle')
-  const navToggleRow = navToggle.getElementsByTagName('span')
   navToggle.addEventListener('click',() => {
     navToggle.classList.toggle('open')
     navRow.classList.toggle('open')
