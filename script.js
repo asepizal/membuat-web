adff8b03086f8eb4637528a3a0b30692c530fb0b
asepizal <aseprizal2496@gmail.com>
2023-11-16 21:04:47 +0700

    mobile style

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
     navToggle.classList.toggle('open')
     navRow.classList.toggle('open')
   })
+
+  function changeNavbarStyle() {
+    const navbar = document.querySelector('.nav')
+    const scrollY = window.scrollY
+
+    if(scrollY > 40) {
+      navbar.style.backgroundColor = '#000000cc'
+    } else {
+      navbar.style.backgroundColor = 'transparent'
+    }
+  }
+
+  window.addEventListener('scroll', changeNavbarStyle)
 });
